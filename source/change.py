9b7104d6d78e80deb9409f0bdfcd88a72d16aeaa
Nivaen2014 <nivaen22@gmail.com>
2026-04-19 13:02:07 -0400

    removed todos

--- source/change.py
+++ source/change.py
@@ -1,15 +1,15 @@
 import sys
 
-# 1. Get inputs from user TODO
+# 1. Get inputs from user 
 cost_of_item = float(input("What is the total cost: "))
 money_from_user = float(input("Give the exact amount or more for this cost: "))
 
 
-# 2. Check if they make sense TODO
+# 2. Check if they make sense 
 if cost_of_item < 0 or money_from_user < 0:
     sys.exit("The cost or the given amount of money is negative")
 
-# 3. Calculate the total change and print TODO
+# 3. Calculate the total change and print
 change = money_from_user - cost_of_item 
 
 # 4. Find the correct bills/coins to give for change TODO
